de03303368d277e87a2412cc7d52bb55ca397c07
tvairakt <t.vairaktaris@gmail.com>
2026-05-28 08:49:37 +0300

    chore(data): remove console.log from fetchHeroes catch

--- src/data.js
+++ src/data.js
@@ -20,8 +20,7 @@ export async function fetchHeroes() {
         heroesCache = data
 
         return heroesCache
-    } catch (error) {
-        console.log(error)
+    } catch {
         return null
     }
 }
